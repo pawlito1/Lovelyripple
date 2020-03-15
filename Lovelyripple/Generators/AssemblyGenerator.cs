36f2c4e4f5570138354f5e76d27b9930e9970496
tpawlicki <tomek996@outlook.com>
2020-03-15 13:33:46 +0100

    remote init

---
+++ Lovelyripple/Generators/AssemblyGenerator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.CodeDom.Compiler;
+
+namespace Lovelyripple.Generators
+{
+    [Serializable]
+    public class AssemblyGenerator
+    {
+        public AssemblyGenerator()
+        {
+        }
+
+        private static string[] GetCommonAssemblies()
+        {
+            return new[]
+            {
+                "Accessibility.dll",
+                "Microsoft.CSharp.dll",
+                "System.Activities.dll",
+                "System.Activities.Presentation.dll",
+                "System.ComponentModel.dll",
+                "System.Configuration.dll",
+                "System.Configuration.Install.dll",
+                "System.Core.dll",
+                "System.Data.dll",
+                "System.Data.DataSetExtensions.dll",
+                "System.Data.Linq.dll",
+                "System.Data.OracleClient.dll",
+                "System.Deployment.dll",
+                "System.Design.dll",
+                "System.DirectoryServices.dll",
+                "System.dll",
+                "System.Drawing.Design.dll",
+                "System.Drawing.dll",
+                "System.EnterpriseServices.dll",
+                "System.Management.dll",
+                "System.Messaging.dll",
+                "System.Runtime.Remoting.dll",
+                "System.Runtime.Serialization.dll",
+                "System.Runtime.Serialization.Formatters.Soap.dll",
+                "System.Security.dll",
+                "System.ServiceModel.dll",
+                "System.ServiceModel.Web.dll",
+                "System.ServiceProcess.dll",
+                "System.Transactions.dll",
+                "System.Web.dll",
+                "System.Web.Extensions.Design.dll",
+                "System.Web.Extensions.dll",
+                "System.Web.Mobile.dll",
+                "System.Web.RegularExpressions.dll",
+                "System.Web.Services.dll",
+                "System.Windows.Forms.Dll",
+                "System.Workflow.Activities.dll",
+                "System.Workflow.ComponentModel.dll",
+                "System.Workflow.Runtime.dll",
+                "System.Xaml.dll",
+                "System.Xml.dll",
+                @"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5.2\WindowsBase.dll",
+                @"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5.2\PresentationCore.dll",
+                @"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5.2\PresentationFramework.dll",
+                "System.Xml.Linq.dll"
+            };
+        }
+    }
+}
---
+++ Lovelyripple/Generators/PackageGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Lovelyripple.Models;
+
+namespace Lovelyripple.Generators
+{
+    [Serializable]
+    public class PackageGenerator
+    {
+        public string Id;
+        public string Description;
+        public Version Version;
+        public string Authors;
+        public string Owners;
+        public string LicenseUrl;
+        public string ProjectUrl;
+        public string IconUrl;
+        public bool RequireLicenseAcceptance;
+        public string ReleaseNotes;
+        public string Copyright;
+        public string Tags;
+        public string ModelsAssemblyGuid;
+        public List<ClassModel> ClassModels;
+        public DirectorySetGenerator DirectorySet;
+
+        string _guid;
+
+        public PackageGenerator()
+        {
+            _guid = System.Guid.NewGuid().ToString();
+            ClassModels = new List<ClassModel>();
+        }
+        public void Build()
+        {
+            CodeGenerator.SaveTo = new System.IO.FileInfo(Path.Combine(DirectorySet.WorkingDirectory,Id+".cs"));
+            CodeGenerator.CreateSourceCode(ClassModels);
+            
+        }
+
+        public static Version CreateFullVersionFromMajorMinor(int majorVersion, int minorVersion)
+        {
+            var cachedNow = DateTime.Now;
+            int buildNumber = (cachedNow.Date - new DateTime(2000, 1, 1)).Days;
+            int revisionNumber = (int)(cachedNow - cachedNow.Date).TotalSeconds / 2;
+            return new Version(majorVersion, minorVersion, buildNumber, revisionNumber);
+        }
+    }
+}
